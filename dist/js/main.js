f8028ef0ea053ac9acb9208469a61ee4dd3c5b9f
Trigo YODILA <trigoyodila1996@gmail.com>
2022-07-07 14:47:23 +0200

    modification menu de navigation

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,15 +4,23 @@ const hamburger = document.querySelector('.menu-btn__burger')
 let showMenu = false
 
 menuBtn.addEventListener('click',toggleMenu)
+const nav = document.querySelector('.nav')
+const menuNav = document.querySelector('.menu-nav')
+const navItems = document.querySelectorAll('.menu-nav__item')
 
 function toggleMenu(){
     if(!showMenu){
         hamburger.classList.add('open')
+        nav.classList.add('open')
+        menuNav.classList.add('open')
+        navItems.forEach(item => item.classList.add('open'))
 
         showMenu = true
     }else{
         hamburger.classList.remove('open')
-
+        nav.classList.remove("open");
+        menuNav.classList.remove("open");
+        navItems.forEach((item) => item.classList.remove("open"));
         showMenu = false
     }
 }
